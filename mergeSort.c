f71c2961983ffb7ad2a94c04624918ae582dbfe8
Andre_XPG <Lycanites@outlook.com>
2026-04-28 01:35:06 -0600

    Algoritmo completado esperando pruebas

--- mergeSort.c
+++ mergeSort.c
@@ -1,70 +1,82 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
-int llenarArray(int *arr, int tamaño)
+int llenarArrayManual(double *arr, int size)
 {
-    for (int i = 0; i < tamaño; i++)
+    for (int i = 0; i < size; i++)
     {
         printf("Ingrese el elemento %d: ", i + 1);
-        scanf("%d", &arr[i]);
+        if (scanf("%lf", &arr[i]) != 1)
+        {
+            fprintf(stderr, "Error al leer el elemento %d\n", i + 1);
+            i--;
+            while (getchar() != '\n')
+                ;
+        }
     }
     return 0;
 }
 
-int merge(int *arr, int *mid, int *fin)
+int llenarArrayAuto(double *arr, int size)
+{
+    srand(time(NULL));
+    for (int i = 0; i < size; i++)
+    {
+        // Genera doubles aleatorios entre -100000.0 y 100000.0
+        arr[i] = ((double)rand() / RAND_MAX) * 200000.0 - 100000.0;
+    }
+    printf("Array llenado automaticamente con %d numeros aleatorios.\n", size);
+    return 0;
+}
+
+int merge(double *arr, double *mid, double *fin)
 {
     int leftSize = mid - arr;
     int rightSize = fin - mid;
 
-    int *left = (int *)malloc(leftSize * sizeof(int));
-    int *right = (int *)malloc(rightSize * sizeof(int));
+    double *left = (double *)malloc(leftSize * sizeof(double));
+    double *right = (double *)malloc(rightSize * sizeof(double));
 
-    memcpy(left, arr, leftSize * sizeof(int));
-    memcpy(right, mid, rightSize * sizeof(int));
+    if (!left || !right)
+    {
+        fprintf(stderr, "Error de memoria en merge\n");
+        free(left);
+        free(right);
+        return -1;
+    }
 
-    int *l = left, *r = right, *f = arr;
+    memcpy(left, arr, leftSize * sizeof(double));
+    memcpy(right, mid, rightSize * sizeof(double));
+
+    double *l = left, *r = right, *f = arr;
 
     while (l < left + leftSize && r < right + rightSize)
     {
         if (*l <= *r)
-        {
-            *f = *l;
-            l++;
-        }
+            *f++ = *l++;
         else
-        {
-            *f = *r;
-            r++;
-        }
-        f++;
+            *f++ = *r++;
     }
 
     while (l < left + leftSize)
-    {
-        *f = *l;
-        l++;
-        f++;
-    }
+        *f++ = *l++;
 
     while (r < right + rightSize)
-    {
-        *f = *r;
-        r++;
-        f++;
-    }
+        *f++ = *r++;
 
     free(left);
     free(right);
     return 0;
 }
 
-int mergeSort(int *inicio, int *final)
+int mergeSort(double *inicio, double *final)
 {
     if (final - inicio <= 1)
-        return 1;
+        return 0;
 
-    int *mid = inicio + (final - inicio) / 2;
+    double *mid = inicio + (final - inicio) / 2;
 
     mergeSort(inicio, mid);
     mergeSort(mid, final);
@@ -72,53 +84,134 @@ int mergeSort(int *inicio, int *final)
     return 0;
 }
 
-int imprimir(int *inicio, int *final)
+void imprimir(double *inicio, double *final)
 {
-    for (int *p = inicio; p < final; p++)
+    int idx = 1;
+    for (double *p = inicio; p < final; p++, idx++)
+        printf("[%d] %.6f\n", idx, *p);
+}
+
+// Submenú reutilizable para pedir tamaño y reservar memoria
+// Retorna el nuevo tamaño, o 0 si hubo error
+int pedirTamanioYAllocar(double **arr)
+{
+    int n;
+    printf("Ingrese el tamanio del array: ");
+    if (scanf("%d", &n) != 1 || n <= 0)
     {
-        printf("%d", *p);
-        printf("\n");
+        printf("Entrada invalida. Por favor, ingrese un numero entero positivo.\n");
+        while (getchar() != '\n')
+            ;
+        return 0;
     }
+    while (getchar() != '\n')
+        ;
+
+    free(*arr);
+    *arr = (double *)malloc(n * sizeof(double));
+    if (!*arr)
+    {
+        printf("Error de memoria\n");
+        return 0;
+    }
+    return n;
 }
 
 int main()
 {
-    int arr[10];
-    int *inicio = arr;
-    int *final = arr + 10;
-    int opc;
+    int n = 0;
+    double *arr = NULL;
+    int opc, subOpc;
 
     while (1)
     {
-        printf("\nSeleccione una opción:\n");
+        printf("\n===== MENU =====\n");
         printf("1. Llenar el array\n");
         printf("2. Ordenar el array\n");
         printf("3. Imprimir el array\n");
         printf("4. Salir\n");
+        printf("Opcion: ");
 
-        scanf("%d \n", &opc);
+        if (scanf("%d", &opc) != 1)
+        {
+            printf("Entrada invalida.\n");
+            while (getchar() != '\n')
+                ;
+            continue;
+        }
+        while (getchar() != '\n')
+            ;
 
         switch (opc)
         {
         case 1:
-            llenarArray(arr, 10);
+            printf("\n--- Llenar Array ---\n");
+            printf("  1. Ingresar numeros manualmente\n");
+            printf("  2. Generar numeros automaticamente\n");
+            printf("  Opcion: ");
+
+            if (scanf("%d", &subOpc) != 1)
+            {
+                printf("Entrada invalida.\n");
+                while (getchar() != '\n')
+                    ;
+                break;
+            }
+            while (getchar() != '\n')
+                ;
+
+            if (subOpc == 1)
+            {
+                n = pedirTamanioYAllocar(&arr);
+                if (n > 0)
+                {
+                    llenarArrayManual(arr, n);
+                    printf("Array llenado manualmente con %d elementos (double).\n", n);
+                }
+            }
+            else if (subOpc == 2)
+            {
+                n = pedirTamanioYAllocar(&arr);
+                if (n > 0)
+                    llenarArrayAuto(arr, n);
+            }
+            else
+            {
+                printf("Opcion invalida.\n");
+            }
             break;
 
         case 2:
-            mergeSort(inicio, final);
-            printf("Array ordenado\n");
+            if (!arr || n <= 0)
+            {
+                printf("Primero llene el array (opcion 1).\n");
+                break;
+            }
+
+            clock_t start = clock();
+            mergeSort(arr, arr + n);
+            clock_t end = clock();
+
+            double tiempo = ((double)(end - start)) / CLOCKS_PER_SEC;
+            printf("Array ordenado en %.6f segundos.\n", tiempo);
             break;
 
         case 3:
-            imprimir(inicio, final);
+            if (!arr || n <= 0)
+            {
+                printf("Primero llene el array (opcion 1).\n");
+                break;
+            }
+            imprimir(arr, arr + n);
             break;
 
         case 4:
             printf("Saliendo...\n");
+            free(arr);
             return 0;
 
         default:
-            printf("Opción inválida\n");
+            printf("Opcion invalida. Intente de nuevo.\n");
         }
     }
 }
